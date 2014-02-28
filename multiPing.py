dd4649c0fa2264b91d8fe738979ef4ba0feb171b
zagfox <feng.zhu1991@gmail.com>
2014-02-27 17:42:16 -0800

    add traffic.py

---
+++ multiPing.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2
+FRONTEND_IP="192.168.126.146"
+CONTROLLER="192.168.126.146"
+WORKERS=["worker1","worker2"]
+
+import sys
+sys.path.append("..")
+import maxinet
+#import examples.topo as topo
+import time
+from fatTree import FatTree
+from thinFT import thinFT
+from Linear import Linear
+from mininet.node import OVSSwitch, UserSwitch
+
+#topo = thinFT(4,8000,0.0)
+#topo = FatTree(4,8000,0.0)
+topo = Linear(2,2,8000,0.0)
+cluster = maxinet.Cluster(FRONTEND_IP, 9090, *WORKERS)
+cluster.start()
+
+exp = maxinet.Experiment(cluster, topo, controller=CONTROLLER, switch=OVSSwitch)
+exp.setup()
+
+print "waiting 3 seconds for routing algorithms on the controller to converge"
+time.sleep(3)
+
+print "Start test here"
+print exp.get_node("h1").cmd("ping -c 5 10.0.0.3")
+time.sleep(2)
+print exp.get_node("h3").cmd("iperf -s &")
+print exp.get_node("h1").cmd("iperf -M 1400 -t 10 -c 10.0.0.3 > ~/results/iperf_h1.txt &")
+print exp.get_node("h2").cmd("iperf -M 1400 -t 10 -c 10.0.0.3 > ~/results/iperf_h2.txt &")
+
+print "wait for hosts to complete iperf"
+time.sleep(15)
+
+exp.stop()
---
+++ traffic.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2
+"""
+traffic.py: test different traffic patterns for link
+"""
+
+import os
+import sys
+sys.path.append("..")
+import maxinet
+import time
+from thinFT import thinFT
+from Linear import Linear
+from mininet.node import OVSSwitch, UserSwitch
+
+def trafficTest(srcNum, dstNum, exp, seconds ):
+	print "Start %d, %d traffic test here" %(srcNum, dstNum)
+	resultPath = "/home/maxinet/maxinet/Frontend/maxinetScript/Results/%d%dtraffic/" %(srcNum, dstNum)
+	if not os.path.exists(resultPath): os.makedirs(resultPath)
+	for i in range(dstNum):
+		exp.get_node("h"+str(i+5)).cmd("iperf -s &")
+	iperfCmd = "iperf -M 1400 -t %d -c 10.0.0." %seconds
+	if dstNum is 1:
+		for i in range(srcNum):
+			exp.get_node("h"+str(i+1)).cmd(iperfCmd+"5"+" > "+resultPath+"iperf_h%d.txt &" % (i+1))
+	else:
+		for i in range(dstNum):
+			exp.get_node("h"+str(i+1)).cmd(iperfCmd+str(i+5)+" > "+resultPath+"iperf_h%d.txt &" % (i+1))
+
+	print "wait for hosts to complete iperf"
+	time.sleep(seconds+5)
+
+
+if __name__ == '__main__':
+	FRONTEND_IP="192.168.126.146"
+	CONTROLLER="192.168.126.146"
+	WORKERS=["worker1"]#,"worker2"]
+
+	#topo = thinFT(4,8000,0.0)
+	#topo = FatTree(4,8000,0.0)
+	topo = Linear(2,4,100,0.0)
+	cluster = maxinet.Cluster(FRONTEND_IP, 9090, *WORKERS)
+	cluster.start()
+
+	exp = maxinet.Experiment(cluster, topo, controller=CONTROLLER, switch=OVSSwitch)
+	exp.setup()
+
+	print "waiting 3 seconds for routing algorithms on the controller to converge"
+	time.sleep(3)
+
+	trafficTest(1, 1, exp, 20)
+	#trafficTest(4, 1, exp, 20)
+	#trafficTest(4, 4, exp, 20)
+
+	exp.stop()
